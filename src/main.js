fbbc938dc8cb3833966e5d6e56fe5f5648dea157
Brandon Pachuca <bpachuca1@gmail.com>
2022-10-31 06:33:33 -0400

    add pinia store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
+
+// TAILWIND
 import "./style/index.css";
+
+// APP ROOT
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import { createPinia } from "pinia";
+const pinia = createPinia();
+
+// CREATE APP
+const app = createApp(App);
+
+app.use(pinia);
+app.mount("#app");
---
+++ src/store/main.js
@@ -0,0 +1,5 @@
+export const useMainStore = defineStore("main", {
+  state: () => ({}),
+  getters: {},
+  actions: {},
+});

16c95b2a82ff014e8fde41be15c1c7c57d2f413f
Brandon Pachuca <bpachuca1@gmail.com>
2022-10-31 06:33:40 -0400

    u

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted } from "vue";
 
 // MAPBOX
-import mapboxgl from 'mapbox-gl'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 // GLOBALS
 let map;
@@ -13,34 +13,30 @@ let map;
  */
 onMounted(() => {
   mapboxgl.accessToken =
-    'pk.eyJ1Ijoia3BmdWkiLCJhIjoiY2p6MWcxMXl4MDFlbTNsbDc1bnp6N3FjYSJ9.66qFOXwI661MOPOf7x96yA'
+    "pk.eyJ1Ijoia3BmdWkiLCJhIjoiY2p6MWcxMXl4MDFlbTNsbDc1bnp6N3FjYSJ9.66qFOXwI661MOPOf7x96yA";
   map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/kpfui/ckn9c157p02a618rzglws1mum',
+    container: "map",
+    style: "mapbox://styles/kpfui/ckn9c157p02a618rzglws1mum",
     zoom: 10,
-    center: [-73.997378, 40.730909]
-  })
-  map.on('load', () => {
-    console.log('map loaded')
-
+    center: [-73.997378, 40.730909],
+  });
+  map.on("load", () => {
     /**
      * TODO:
-     * 
+     *
      * Add pluto dataset
-     * 
+     *
      * Add polygon listener
      */
-  })
-})
-
-
-
+  });
+});
 </script>
 
 <template>
-  <div id="map" class="absolute h-screen w-screen top-0 left-0 overflow-hidden"></div>
+  <div
+    id="map"
+    class="absolute h-screen w-screen top-0 left-0 overflow-hidden"
+  ></div>
 </template>
 
-<style>
-
-</style>
+<style></style>

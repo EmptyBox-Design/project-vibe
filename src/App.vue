fb599b777519390e7f3d63377900d7912d27707d
Brandon <bpachuca1@gmail.com>
2022-10-31 06:09:11 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from "./components/Home.vue"
+
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { onMounted } from 'vue';
+
+// MAPBOX
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+// GLOBALS
+let map;
+
+/**
+ * MOUNTED
+ */
+onMounted(() => {
+  mapboxgl.accessToken =
+    'pk.eyJ1Ijoia3BmdWkiLCJhIjoiY2p6MWcxMXl4MDFlbTNsbDc1bnp6N3FjYSJ9.66qFOXwI661MOPOf7x96yA'
+  map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/kpfui/ckn9c157p02a618rzglws1mum',
+    zoom: 10,
+    center: [-73.997378, 40.730909]
+  })
+  map.on('load', () => {
+    console.log('map loaded')
+
+    /**
+     * TODO:
+     * 
+     * Add pluto dataset
+     * 
+     * Add polygon listener
+     */
+  })
+})
+
+
+
+</script>
+
+<template>
+  <div id="map" class="absolute h-screen w-screen top-0 left-0 overflow-hidden"></div>
+</template>
+
+<style>
+
+</style>

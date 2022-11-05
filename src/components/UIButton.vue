4992701eb1d15179060427c3df04445495800fe1
Atharva Ranade <ARanade@nbbj.com>
2022-11-05 13:01:02 -0700

    basic button setup

---
+++ src/components/UIButton.vue
@@ -0,0 +1,18 @@
+<template>
+<form>
+  <h1>Please select a walking radius</h1>
+  <ul>
+    <li><label for="1"><input type="radio" id="1" name="question1" />5 miles</label></li>
+    <li><label for="2"><input type="radio" id="2" name="question1" />10 miles</label></li>  
+  </ul>
+  <div class="apply">
+    <button disabled class="btn" @click = "" >Supply</button>
+  </div>
+</form>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css" scoped src='./UIButton.css' />
